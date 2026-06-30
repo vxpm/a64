c1d4daa29742311e6830fa1c3f3d508dd5c014d1
vinícius x <viniciusximenespm@gmail.com>
2026-06-30 14:55:57 -0300

    branch sys

--- a64/src/branch_sys.rs
+++ a64/src/branch_sys.rs
@@ -10,35 +10,39 @@ pub enum Instruction {}
 impl Instruction {
     pub fn new(value: u32) -> Option<Self> {
         let op0 = value.bits(29, 32);
-        let op1 = value.bits(5, 25);
+
+        // op1: 12..26, split to generate better code with bit_match
+        let op1_a = value.bits(16, 26);
+        let op1_b = value.bits(12, 16);
+
         let op2 = value.bits(0, 5);
 
         Some(bit_match! {
-            match (op0, op1, op2) {
-                ("010", "00____________", "_____") => todo!("cond branch (immediate)"),
-                ("010", "01____________", "_____") => todo!("misc branch (immediate)"),
+            match (op0, op1_a, op1_b, op2) {
+                ("010", "00________", "____", "_____") => todo!("cond branch (immediate)"),
+                ("010", "01________", "____", "_____") => todo!("misc branch (immediate)"),
 
-                ("011", "00________1___", "_____") => todo!("cmp u8/u16 reg and branch"),
+                ("011", "00________", "1___", "_____") => todo!("cmp u8/u16 reg and branch"),
 
-                ("110", "00____________", "_____") => todo!("except"),
-                ("110", "01000000110001", "_____") => todo!("sys with reg"),
-                ("110", "01000000110010", "11111") => todo!("hints"),
-                ("110", "01000000110011", "_____") => todo!("barriers"),
-                ("110", "0100000___0100", "_____") => todo!("pstate"),
-                ("110", "0100_01_______", "_____") => todo!("sys"),
-                ("110", "0100_1________", "_____") => todo!("sys reg move"),
-                ("110", "0101_01_______", "_____") => todo!("sys pair"),
-                ("110", "0101_1________", "_____") => todo!("sys reg pair move"),
-                ("110", "1_____________", "_____") => todo!("uncond branch (reg)"),
+                ("110", "00________", "____", "_____") => todo!("except"),
+                ("110", "0100000011", "0001", "_____") => todo!("sys with reg"),
+                ("110", "0100000011", "0010", "11111") => todo!("hints"),
+                ("110", "0100000011", "0011", "_____") => todo!("barriers"),
+                ("110", "0100000___", "0100", "_____") => todo!("pstate"),
+                ("110", "0100_01___", "____", "_____") => todo!("sys"),
+                ("110", "0100_1____", "____", "_____") => todo!("sys reg move"),
+                ("110", "0101_01___", "____", "_____") => todo!("sys pair"),
+                ("110", "0101_1____", "____", "_____") => todo!("sys reg pair move"),
+                ("110", "1_________", "____", "_____") => todo!("uncond branch (reg)"),
 
-                ("_00", "______________", "_____") => todo!("uncond branch (imm)"),
+                ("_00", "__________", "____", "_____") => todo!("uncond branch (imm)"),
 
-                ("_01", "0_____________", "_____") => todo!("cmp and branch (imm)"),
-                ("_01", "1_____________", "_____") => todo!("test and branch (imm)"),
+                ("_01", "0_________", "____", "_____") => todo!("cmp and branch (imm)"),
+                ("_01", "1_________", "____", "_____") => todo!("test and branch (imm)"),
 
-                ("_11", "00________00__", "_____") => todo!("cmp regs and branch"),
-                ("_11", "01_________0__", "_____") => todo!("cmp reg with imm and branch"),
-                _ => todo!(),
+                ("_11", "00________", "00__", "_____") => todo!("cmp regs and branch"),
+                ("_11", "01________", "_0__", "_____") => todo!("cmp reg with imm and branch"),
+                _ => return None,
             }
         })
     }
