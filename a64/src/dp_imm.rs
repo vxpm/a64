82f738403107005771a5b015538f681eb6eabc48
vinícius x <viniciusximenespm@gmail.com>
2026-06-30 13:59:53 -0300

    mov wide

--- a64/src/dp_imm.rs
+++ a64/src/dp_imm.rs
@@ -3,10 +3,12 @@ use bitos::BitUtils;
 use derive_more::Display;
 
 pub mod add_sub;
+pub mod mov_wide;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Display)]
 pub enum Instruction {
     AddSub(add_sub::Instruction),
+    MovWide(mov_wide::Instruction),
 }
 
 impl Instruction {
@@ -16,7 +18,15 @@ impl Instruction {
 
         Some(bit_match! {
             match (op0, op1) {
+                ("11", "111_") => todo!("one src"),
+                ("__", "00__") => todo!("pc rel addressing"),
                 ("__", "010_") => Instruction::AddSub(add_sub::Instruction::new(value)?),
+                ("__", "0110") => todo!("add sub with tags"),
+                ("__", "0111") => todo!("min max"),
+                ("__", "100_") => todo!("logical"),
+                ("__", "101_") => Instruction::MovWide(mov_wide::Instruction::new(value)?),
+                ("__", "110_") => todo!("bitfield"),
+                ("__", "111_") => todo!("extract"),
                 _ => return None,
             }
         })
---
+++ a64/src/dp_imm/mov_wide.rs
@@ -0,0 +1,63 @@
+use core::fmt::Display;
+
+use a64_macros::bit_match;
+use bitos::integer::u2;
+use bitos::{BitUtils, bitos};
+use derive_more::Display;
+
+use crate::Xr;
+
+/// Move wide with zero
+///
+/// This instruction moves an optionally-shifted 16-bit immediate value to a register.
+#[bitos(32)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Movz64 {
+    /// Destination register.
+    #[bits(0..5)]
+    pub rd: Xr,
+    /// Immediate value.
+    #[bits(5..21)]
+    pub imm: u16,
+    /// Amount by which to shift the immediate left, divided by 16.
+    #[bits(21..23)]
+    pub hw: u2,
+}
+
+impl Display for Movz64 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "MOVZ {}, #{}, LSL #{}",
+            self.rd(),
+            self.imm(),
+            self.hw().value() * 16,
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Display)]
+pub enum Instruction {
+    Movz64(Movz64),
+}
+
+impl Instruction {
+    pub fn new(value: u32) -> Option<Self> {
+        let sf = value.bit(31) as u32;
+        let opc = value.bits(29, 31);
+        let hw = value.bits(21, 23);
+
+        Some(bit_match! {
+            match (sf, opc, hw) {
+                ("0", "00", "0_") => todo!("movn 32 bit"),
+                ("0", "10", "0_") => todo!("movz 32 bit"),
+                ("0", "11", "0_") => todo!("movk 32 bit"),
+
+                ("1", "00", "__") => todo!("movn 64 bit"),
+                ("1", "10", "__") => Self::Movz64(Movz64(value)),
+                ("1", "11", "__") => todo!("movk 64 bit"),
+                _ => return None,
+            }
+        })
+    }
+}
